af152983d24b7ccf9637005105ba5979f40a8193
Bhavanasree <shreebhavana9@gmail.com>
2026-03-19 15:45:00 +0530

    MA-Initial commit

---
+++ agents/__init__.py
@@ -0,0 +1,5 @@
+from agents.strategy_agent import create_marketing_strategy
+
+tools=[
+    create_marketing_strategy
+]
---
+++ graph/state.py
@@ -0,0 +1,5 @@
+from langgraph.graph import MessagesState
+
+class AgentState(MessagesState):
+    """State class for agents to carry the conversation history and other relevant information."""
+    pass
